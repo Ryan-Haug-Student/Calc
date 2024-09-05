93f37af6051ee1cde7cade06103db0c40aea5be5
Ryan-Haug-Student <ryha601@student.oaisd.org>
2024-09-05 20:23:33 +0000

    made all inputs working, except addition

--- Calc.js
+++ Calc.js
@@ -1,6 +1,27 @@
-const button = document.querySelector('#AddSolve')
+const abutton = document.querySelector('#AddSolve')
 const add1 = document.querySelector('#Addinput1')
 const add2 = document.querySelector('#Addinput2')
 
-button.addEventListener('click', () =>
-{alert(add1.value + add2.value)})
+const sbutton = document.querySelector('#SubSolve')
+const sub1 = document.querySelector('#Subinput1')
+const sub2 = document.querySelector('#Subinput2')
+
+const mbutton = document.querySelector('#MulSolve')
+const mul1 = document.querySelector('#Mulinput1')
+const mul2 = document.querySelector('#Mulinput2')
+
+const dbutton = document.querySelector('#DivSolve')
+const div1 = document.querySelector('#Divinput1')
+const div2 = document.querySelector('#Divinput2')
+
+abutton.addEventListener('click', () =>
+    {alert(add1.value + add2.value)})
+
+sbutton.addEventListener('click', () =>
+    {alert(sub1.value - sub2.value)})
+
+mbutton.addEventListener('click', () =>
+    {alert(mul1.value * mul2.value)})
+
+dbutton.addEventListener('click', () =>
+    {alert(div1.value / div2.value)})
